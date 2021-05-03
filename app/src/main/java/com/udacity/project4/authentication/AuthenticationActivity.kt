1a16f9027eb054939f17ab95c9703d4c506944f4
isosic <ivan.sosic@asseco-see.hr>
2021-05-03 16:02:18 +0200

    added permission request before navigating

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,15 +1,21 @@
 package com.udacity.project4.authentication
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -18,9 +24,16 @@ import com.udacity.project4.databinding.ActivityAuthenticationBinding
 class AuthenticationActivity : AppCompatActivity() {
 
     companion object {
-        const val SIGN_IN_REQUEST_CODE = 1001
+        private const val SIGN_IN_REQUEST_CODE = 1001
+        private const val REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSION_RESULT_CODE = 33
+        private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
+        private const val REQUEST_TURN_DEVICE_LOCATION_ON = 29
+        private const val LOCATION_PERMISSION_INDEX = 0
+        private const val BACKGROUND_LOCATION_PERMISSION_INDEX = 1
     }
 
+    private var runningQOrLater = android.os.Build.VERSION.SDK_INT >=
+            android.os.Build.VERSION_CODES.Q
 
     private lateinit var binding: ActivityAuthenticationBinding
     private lateinit var viewModel: AuthenticationViewModel
@@ -30,13 +43,27 @@ class AuthenticationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         viewModel =
-            ViewModelProvider(this, defaultViewModelProviderFactory).get(AuthenticationViewModel::class.java)
+            ViewModelProvider(
+                this,
+                defaultViewModelProviderFactory
+            ).get(AuthenticationViewModel::class.java)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
         binding.activityAuthenticationButton.setOnClickListener { launchSignInFlow() }
 
 
-        setContentView(binding.root)
+//        viewModel.authenticationState.observe(this, Observer { state ->
+//            when(state){
+//                AuthenticationViewModel.AuthenticationState.AUTHENTICATED -> {
+//                    startActivity(Intent(this, RemindersActivity.class))
+//                }
+//                else -> {}
+//            }
+//
+//        } )
+
+
+//        setContentView(binding.root)
 //         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
 
 //          TODO: If the user was authenticated, send him to RemindersActivity
@@ -60,11 +87,75 @@ class AuthenticationActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == SIGN_IN_REQUEST_CODE){
-            val response = IdpResponse.fromResultIntent(data)
-            if(resultCode == Activity.RESULT_OK){
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+//            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                checkPermissionsAndProceed()
+//                startActivity(Intent(this, RemindersActivity::class.java))
+//                finish()
 //                findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToSaveReminderFragment())
             }
         }
+    }
+
+    private fun checkPermissionsAndProceed() {
+        if (!permissionsGranted()) {
+            requestPermissions()
+        }
+
+    }
+
+
+    private fun permissionsGranted(): Boolean {
+        val foregroundLocationApproved = (
+                PackageManager.PERMISSION_GRANTED ==
+                        ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ))
+        val backgroundPermissionApproved =
+            if (runningQOrLater) {
+                PackageManager.PERMISSION_GRANTED ==
+                        ActivityCompat.checkSelfPermission(
+                            this, Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                        )
+            } else {
+                true
+            }
+        return foregroundLocationApproved && backgroundPermissionApproved
+    }
+
+    private fun requestPermissions() {
+        var permissionArray = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+        var resultCode = when {
+            runningQOrLater -> {
+                permissionArray += Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSION_RESULT_CODE
+            }
+            else -> REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE
+        }
+        ActivityCompat.requestPermissions(this, permissionArray, resultCode)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (grantResults.isEmpty() ||
+            grantResults[LOCATION_PERMISSION_INDEX] == PackageManager.PERMISSION_DENIED ||
+            (requestCode == REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSION_RESULT_CODE &&
+                    grantResults[BACKGROUND_LOCATION_PERMISSION_INDEX] ==
+                    PackageManager.PERMISSION_DENIED)
+        ) {
+            Toast.makeText(this, "You cannot deny me!", Toast.LENGTH_SHORT).show()
+        } else {
+            navigateAway()
+        }
+    }
+
+    private fun navigateAway() {
+        startActivity(Intent(this, RemindersActivity::class.java))
+        finish()
     }
 }
